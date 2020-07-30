cf4f71b69242c365eb9dc24852ae6cb7b18c7682
dansirdan <daniel.monteton@gmail.com>
2020-07-30 09:19:05 -0700

    Folder structure and react app created

--- client/src/App.js
+++ client/src/App.js
@@ -4,29 +4,6 @@ import { Home, Gallery, About, Contact, NoMatch } from './pages';
 // import Nav from './components/Nav';
 import './App.css';
 
-const name = "Roger";
-const thoughts = ["React is okay, I guess.", "I want to play Horizon Zero Dawn"];
-
-function JSXVariables() {
-  // const name2 = "Karen";
-  return (
-    <div className="main-container">
-      <div className="container">
-        <div className="jumbotron">
-          <h1>Hi! My name is ({name})</h1>
-          <h2>My name has ({name.length}) letters</h2>
-          {
-            thoughts.map((thought) => {
-             return <h2>{thought}</h2>;
-            })
-          }
-          {/* <h2>I think React ({thoughts})</h2> */}
-        </div>
-      </div>
-    </div>
-  );
-}
-
 function App() {
   return (
     <Router>
@@ -38,9 +15,7 @@ function App() {
           <Route exact path='/about' component={About} />
           <Route exact path='/contact' component={Contact} />
           <Route component={NoMatch} />
-
         </Switch>
-        <JSXVariables></JSXVariables>
       </div>
     </Router>
   );
