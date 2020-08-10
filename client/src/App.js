66623522b0f8881851068194bb174e2036cad500
dansirdan <daniel.monteton@gmail.com>
2020-08-10 12:40:20 -0700

    Finalizing Nav choices

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,65 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Home, Gallery, About, Contact, NoMatch, Collection, Detail } from './pages';
-import Nav from './components/Nav';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  Home,
+  Gallery,
+  About,
+  Contact,
+  NoMatch,
+  Collection,
+  Detail,
+} from "./pages";
+import { CssBaseline } from "@material-ui/core";
+import Nav from "./components/Nav";
+import GalleryNav from "./components/GalleryNav";
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/gallery' component={Gallery} />
-          <Route
-            exact path={[
-              '/gallery/watercolor',
-              '/gallery/pastel',
-              '/gallery/portraits',
-              '/gallery/cards',
-              '/gallery/other'
-            ]}
-            render={
-              ({ location }) => {
-                let pathArr = location.pathname.split('/')
-                return <Collection collection={pathArr[2]} />
-              }
-            } />
-          <Route
-            exact path={[
-              '/gallery/watercolor/:id',
-              '/gallery/pastel/:id',
-              '/gallery/portraits/:id',
-              '/gallery/cards/:id',
-              '/gallery/other/:id'
-            ]}
-            render={
-              ({ location }) => {
-                let pathArr = location.pathname.split('/')
-                return <Detail collection={pathArr[2]} id={pathArr[3]} />
-              }
-            }
-          />
-          <Route exact path='/about' component={About} />
-          <Route exact path='/contact' component={Contact} />
-          <Route component={NoMatch} />
-        </Switch>
-      </div>
-    </Router>
+    <React.Fragment>
+      <CssBaseline />
+      <Router>
+        <div>
+          <Nav />
+          <GalleryNav/>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/gallery' component={Gallery} />
+            <Route
+              exact
+              path={[
+                "/gallery/watercolor",
+                "/gallery/pastel",
+                "/gallery/portraits",
+                "/gallery/cards",
+                "/gallery/other",
+              ]}
+              render={({ location }) => {
+                let pathArr = location.pathname.split("/");
+                return <Collection collection={pathArr[2]} />;
+              }}
+            />
+            <Route
+              exact
+              path={[
+                "/gallery/watercolor/:id",
+                "/gallery/pastel/:id",
+                "/gallery/portraits/:id",
+                "/gallery/cards/:id",
+                "/gallery/other/:id",
+              ]}
+              render={({ location }) => {
+                let pathArr = location.pathname.split("/");
+                return <Detail collection={pathArr[2]} id={pathArr[3]} />;
+              }}
+            />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/contact' component={Contact} />
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
+      </Router>
+    </React.Fragment>
   );
 }
 
