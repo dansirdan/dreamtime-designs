9fa53a5a5e246354fcc2c027e628bd79936a6166
dansirdan <daniel.monteton@gmail.com>
2021-01-30 17:27:33 -0800

    pull from main remote

---
+++ client/build/precache-manifest.11dc7b39d07cf829e306027e59f4c936.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb0a19bf39e1a8c6efd847739bc7a779",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1a544575d8d1d7ae352",
+    "url": "/static/css/main.6717af92.chunk.css"
+  },
+  {
+    "revision": "192d87e1e1a8780fac78",
+    "url": "/static/js/2.af2f0dfc.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.af2f0dfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1a544575d8d1d7ae352",
+    "url": "/static/js/main.605d327f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
